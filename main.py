e81bc0d535968cd2629a53ad9140a293aeb824a8
Vulldozer <mhmd.ara56@gmail.com>
2024-01-19 00:27:29 +0200

    add skip to last and first page buttons

--- main.py
+++ main.py
@@ -43,6 +43,19 @@ class PageButtons(discord.ui.View):
         self.subpage_index = 0
         self.max_pages = 0
 
+    @discord.ui.button(emoji="⏪",style=discord.ButtonStyle.green)
+    async def first_button(self, interaction: discord.Interaction, button: discord.ui.Button):
+        page_button = PageButtons()
+        page_button.page_index = 1
+        page_button.subpage_index = 0
+        page_button.max_pages = self.max_pages
+        page_button.previous_button.disabled = True
+        page_button.first_button.disabled = True
+        embed=lb_embed(15,page_button.page_index,page_button.subpage_index)
+        page_button.pages_button.label = str((page_button.page_index-1)*2+page_button.subpage_index+1) + "/" + str(page_button.max_pages)
+        await interaction.response.defer()
+        msg = await interaction.original_response()
+        await msg.edit(embed=embed,view=page_button)
     @discord.ui.button(emoji="◀️",style=discord.ButtonStyle.primary)
     async def previous_button(self, interaction: discord.Interaction, button: discord.ui.Button):
         page_button = PageButtons()
@@ -56,6 +69,7 @@ class PageButtons(discord.ui.View):
             page_button.page_index -= 1
         if page_button.page_index == 1 and page_button.subpage_index == 0:
             page_button.previous_button.disabled = True
+            page_button.first_button.disabled = True
         embed=lb_embed(15,page_button.page_index,page_button.subpage_index)
         page_button.pages_button.label = str((page_button.page_index-1)*2+page_button.subpage_index+1) + "/" + str(page_button.max_pages)
         await interaction.response.defer()
@@ -82,7 +96,22 @@ class PageButtons(discord.ui.View):
         await interaction.response.defer()
         msg = await interaction.original_response()
         await msg.edit(embed=embed,view=page_button)
-
+    @discord.ui.button(emoji="⏩",style=discord.ButtonStyle.green)
+    async def last_button(self, interaction: discord.Interaction, button: discord.ui.Button):
+        page_button = PageButtons()
+        page_button.page_index = int((self.max_pages-1)/2)+1
+        if self.max_pages%2 == 0:
+            page_button.subpage_index = 1
+        else:
+            page_button.subpage_index = 0
+        page_button.max_pages = self.max_pages
+        page_button.next_button.disabled = True
+        page_button.last_button.disabled = True
+        embed=lb_embed(15,page_button.page_index,page_button.subpage_index)
+        page_button.pages_button.label = str((page_button.page_index-1)*2+page_button.subpage_index+1) + "/" + str(page_button.max_pages)
+        await interaction.response.defer()
+        msg = await interaction.original_response()
+        await msg.edit(embed=embed,view=page_button)
 def lb_embed(season,page,subpage):
     lb_url= "https://data.ninjakiwi.com/battles2/homs/season_" + str(season) + "/leaderboard?page=" + str(page)
     response = requests.get(lb_url)
@@ -117,6 +146,7 @@ async def leaderboard_command(interaction: discord.Interaction):
     page_button=PageButtons()
     page_button.page_index = 1
     page_button.previous_button.disabled = True
+    page_button.first_button.disabled = True
     page_button.max_pages = int(math.ceil((homs["body"][0]["totalScores"])/25)) 
     page_button.pages_button.label = "1" + "/" + str(page_button.max_pages)
     await interaction.response.send_message(embed=rankings, view=page_button)
