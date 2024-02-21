2dc3324edad3607d16c0e14b3ab29376d6a0d969
Vulldozer <mhmd.ara56@gmail.com>
2024-02-21 21:56:51 +0200

    player stats command added

--- main.py
+++ main.py
@@ -10,13 +10,13 @@ intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
-api_url = "https://data.ninjakiwi.com/battles2/homs"
-page_index = 1
 
-cute_people = ['Emilplane','Ryan Mehalic','Cologne']
-towers = ['Dart','Tack','Boomerang','Bomb','Ice','Glue','Sniper','Sub','Boat','Ace','Heli','Mortar','Dartling','Wizard','Super','Ninja','Alchemist','Druid','Village','Farm','Spike Factory','Engineer']
-maps = ['Bloontonium Mines','Docks','In the Wall','Mayan','Thin Ice','Banana Depot','Basalt Columns','Bloon Bot Factory','Building Site','Castle Ruins','Cobra Command','Dino Graveyard','Garden','Glade','Inflection','Koru','Oasis','Off-tide','Pirate Cove','Ports','Precious Space','Sands of Time','Star','Sun Palace']
-heroes = ['Quincy','Cyber Quincy','Gwen','Science Gwen','Obyn','Ocean Obyn','Striker Jones','Biker Bones','Churchill','Sentai','Benjamin','DJ Benjammin','Ezili','Smudge Catt Ezili','Pat Fusty','Snowpat','Jericho','Highwayman','Star Captain','Adora']
+@tree.command(
+    name="ping",
+    description="ping the bot",
+)
+async def ping_command(interaction: discord.Interaction):
+    await interaction.response.send_message("Pong! " + str(client.latency*1000) + "ms")
 
 @tree.command(
     name="map",
@@ -24,6 +24,7 @@ heroes = ['Quincy','Cyber Quincy','Gwen','Science Gwen','Obyn','Ocean Obyn','Str
 )
 @app_commands.describe(hom_only="Return only Hall of Masters maps")
 async def map_command(interaction: discord.Interaction, hom_only: Literal["Yes","No"]):
+    maps = ['Bloontonium Mines','Docks','In the Wall','Mayan','Thin Ice','Banana Depot','Basalt Columns','Bloon Bot Factory','Building Site','Castle Ruins','Cobra Command','Dino Graveyard','Garden','Glade','Inflection','Koru','Oasis','Off-tide','Pirate Cove','Ports','Precious Space','Sands of Time','Star','Sun Palace']
     if interaction.data["options"][0]["value"] == "Yes":
         await interaction.response.send_message(maps[random.randint(4,(len(maps) - 1))])
     else:
@@ -34,6 +35,8 @@ async def map_command(interaction: discord.Interaction, hom_only: Literal["Yes",
     description="Randomly generate a tower loadout"
 )
 async def quad_command(interaction):
+    towers = ['Dart','Tack','Boomerang','Bomb','Ice','Glue','Sniper','Sub','Boat','Ace','Heli','Mortar','Dartling','Wizard','Super','Ninja','Alchemist','Druid','Village','Farm','Spike Factory','Engineer']
+    heroes = ['Quincy','Cyber Quincy','Gwen','Science Gwen','Obyn','Ocean Obyn','Striker Jones','Biker Bones','Churchill','Sentai','Benjamin','DJ Benjammin','Ezili','Smudge Catt Ezili','Pat Fusty','Snowpat','Jericho','Highwayman','Star Captain','Adora']
     quad = random.sample(towers, k=3)
     await interaction.response.send_message(quad[0] + ", " + quad[1] + ", " + quad[2] + ", and " + heroes[random.randint(0,(len(heroes) - 1))])
 
@@ -113,12 +116,13 @@ class PageButtons(discord.ui.View):
         await interaction.response.defer()
         msg = await interaction.original_response()
         await msg.edit(embed=embed,view=page_button)
+
 def lb_embed(season,page,subpage):
     lb_url= "https://data.ninjakiwi.com/battles2/homs/season_" + str(season) + "/leaderboard?page=" + str(page)
     response = requests.get(lb_url)
     leaderboard = response.json()
     embed = discord.Embed(
-            title="Season " + str(season) + " leaderboard",
+            title="Season " + str(season) + " leaderboard"
             )
     if leaderboard["success"] == False:
         discord.Embed.add_field(embed,name="No more rankings left",value="last page reached")
@@ -152,9 +156,86 @@ async def leaderboard_command(interaction: discord.Interaction):
     page_button.pages_button.label = "1" + "/" + str(page_button.max_pages)
     await interaction.response.send_message(embed=rankings, view=page_button)
 
+def player_embed(oakurl):
+    player = requests.get(oakurl).json()
+    embed = discord.Embed(
+            title="Player Stats - " + player["body"]["displayName"]
+            )
+    image_url = player["body"]["equippedAvatarURL"]
+    discord.Embed.set_thumbnail(embed, url=image_url)
+    ranked = player["body"]["rankedStats"]
+    ranked_stats = "Wins: " + str(ranked["wins"]) + " \n"
+    ranked_stats += "Losses: " + str(ranked["losses"]) + " \n"
+    ranked_stats += "Draws: " + str(ranked["draws"]) + " \n"
+    ranked_stats += "Winrate: " + "{0:.2f}".format(ranked["wins"]/(ranked["losses"]+ranked["wins"])*100) + "%" + " \n"
+    ranked_stats += "Current Winstreak: " + str(ranked["win_streak"]) + " \n"
+    ranked_stats += "Highest Winstreak: " + str(ranked["highest_win_streak"])
+    discord.Embed.add_field(
+        embed,
+        name="Ranked Stats",
+        value=ranked_stats
+        )
+    return embed
+
+def player_selection(matches):
+    embed = discord.Embed(
+        title="Found multiple results",
+        description="please select which player's stats you want to view")
+    return embed
+
+class PlayerSelect(discord.ui.View):
+    def __init__(self, players):
+        self.players = players  
+        discord.ui.Select(options=players["displayName"])
+
+    async def options(self, interaction: discord.Interaction, select: discord.ui.Select):
+        pass
+        
+@tree.command(
+    name="player",
+    description="Shows a player's stats - must be in hall of masters"
+)
+async def player_command(interaction: discord.Interaction, player: str):
+    player = interaction.data["options"][0]["value"]
+    homs = requests.get("https://data.ninjakiwi.com/battles2/homs").json()
+    last_page = int(math.ceil((homs["body"][0]["totalScores"])/50))
+    found = 0
+    page = 1
+    matches = []
+    while (page <= last_page):
+        lb_url= "https://data.ninjakiwi.com/battles2/homs/season_15/leaderboard?page=" + str(page)
+        response = requests.get(lb_url)
+        leaderboard = response.json()
+        for entry in leaderboard["body"]:
+            if entry["displayName"].lower() == player.lower():
+                matches.append(entry)
+                found += 1
+        page += 1
+    if found == 0:
+        await interaction.response.send_message("I couldn't find this player")
+    elif found == 1:
+        player_stats = player_embed(matches[0]["profile"])
+        await interaction.response.send_message(embed=player_stats)
+    else:
+        embed = discord.Embed(
+        title="Found multiple results",
+        description="please select which player's stats you want to view"
+        )
+        select = discord.ui.Select()
+        for match in matches:
+            select.add_option(label=match["displayName"],value=match["profile"])
+        async def selection_callback(interaction):
+            player_stats = player_embed(select.values[0])
+            await interaction.response.send_message(embed=player_stats)
+        select.callback = selection_callback
+        view = discord.ui.View()
+        view.add_item(select)
+        await interaction.response.send_message(embed=embed,view=view)
+
 @client.event
 async def on_ready():
     await tree.sync()
+    cute_people = ['Emilplane','Ryan Mehalic','Cologne', 'Paytrolah']
     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=cute_people[random.randint(0,len(cute_people)-1)]))
     print("Initilization Complete")
 
