8f4b6bb0c83af7c1572b5fac86c372753f212068
Vulldozer <mhmd.ara56@gmail.com>
2024-02-29 16:21:20 +0200

    described hom_only parameter

--- banana_farmer/commands/map.py
+++ banana_farmer/commands/map.py
@@ -4,6 +4,13 @@ from typing import Literal
 
 
 async def map_callback(interaction, hom_only: Literal["Yes","No"]):
+    """Randomly select a map
+
+    Parameters
+    -----------
+    hom_only: Literal["Yes","No"]
+        Return only Hall of Masters maps?
+    """
     maps = ['Bloontonium Mines','Docks','In the Wall','Mayan','Thin Ice','Banana Depot','Basalt Columns','Bloon Bot Factory','Building Site','Castle Ruins','Cobra Command','Dino Graveyard','Garden','Glade','Inflection','Koru','Oasis','Off-tide','Pirate Cove','Ports','Precious Space','Sands of Time','Star','Sun Palace','Salmon Ladder']
     if interaction.data["options"][0]["value"] == "Yes":
         await interaction.response.send_message(maps[random.randint(4,(len(maps) - 1))])
